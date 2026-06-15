7e6afb8e4d632a1239e89784170a3dbd444eb5aa
asifalam515 <asibulalam515@gmail.com>
2026-06-15 21:29:17 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import jwt from "@fastify/jwt";
 import fastify from "fastify";
 
 const app = fastify({
   logger: true,
 });
+app.register(cors);
+app.register(helmet);
+app.register(jwt);
+
 app.get("/", async (request, reply) => {
   return { hello: "world" };
 });
