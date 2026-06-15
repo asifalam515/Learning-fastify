4cdbbb21eb3d86752bb0d34bf4c158f0659a4a92
asifalam515 <asibulalam515@gmail.com>
2026-06-15 21:32:28 +0600

    issues resolved

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import helmet from "@fastify/helmet";
 import jwt from "@fastify/jwt";
 import fastify from "fastify";
 
+const jwtSecret = process.env.JWT_SECRET ?? "supersecret";
+
 const app = fastify({
   logger: true,
 });
 app.register(cors);
 app.register(helmet);
-app.register(jwt);
+app.register(jwt, { secret: jwtSecret });
 
 app.get("/", async (request, reply) => {
   return { hello: "world" };
